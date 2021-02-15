43b68f2fe222d36fd07930e38f8ae7d5eff1da9a
SmonSE <68323384+SmonSE@users.noreply.github.com>
2021-02-15 14:56:05 +0100

    Update notizBlog_load_json.js

--- notizBlog_load_json.js
+++ notizBlog_load_json.js
@@ -1,146 +1,203 @@
-// Variables used by Scriptable.
+//    Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: red; icon-glyph: magic;
 // notizblog Artikel Update
 
 let param = args.widgetParameter
-if (param != null && param.length > 0) {
-    storeId = param
-}
 
-const widget = new ListWidget()
 const apiData = await getNewCasesData()
 const date = await getDate()
-await createWidget()
 
+const widgetSize = (config.widgetFamily ? 
+// config.widgetFamily : 'small');
+config.widgetFamily : 'large')
+// config.widgetFamily : '');
+const widget = await createWidget();
 
-// used for debugging if script runs inside the app
-if (!config.runsInWidget) {
-    await widget.presentSmall()
+// For debug delete "!" from !config.runInWidget
+if (!config.runInWidget) {
+ switch(widgetSize) {
+   case 'small':
+   await widget.presentSmall();
+   break;
+
+   case 'large':
+   await widget.presentLarge();
+   break;
+
+   default: // medium
+   await widget.presentMedium();
+ }
 }
-Script.setWidget(widget)
-Script.complete()
+Script.setWidget(widget);
+Script.complete();
 
+//------------------------------------------------
 // build the content of the widget
 async function createWidget() {
 
-  widget.addSpacer(4)
-  const logoImg = await getImage('logo.png')
+ const list = new ListWidget();
 
-  widget.setPadding(5, 10, 10, 10)
-  const titleFontSize = 12
-  const detailFontSize = 36
+ let row1 = list.addStack()
+ row1.layoutHorizontally()
+ row1.addSpacer(1)
 
-  const logoStack = widget.addStack()
-  logoStack.addSpacer(90)
-  const logoImageStack = logoStack.addStack()
-  logoStack.layoutHorizontally()
-  logoImageStack.backgroundColor = new Color("#ffffff", 1.0)
-  logoImageStack.cornerRadius = 6
-  const wimg = logoImageStack.addImage(logoImg)
-  wimg.imageSize = new Size(40, 40)
-  wimg.rightAlignImage()
-//   widget.addSpacer()
+ let column1 = row1.addStack()
+ column1.layoutVertically()
 
-  let row = widget.addStack()
-  row.layoutHorizontally()
-  row.addSpacer(1)
+ let column2 = row1.addStack()
+ column2.layoutVertically()
 
-  let column = row.addStack()
-  column.layoutVertically()
+ const logoImg = await getImage('logo.png')
+ const logoStack = column2.addStack()
 
-  const paperText = column.addText("notiz.Blog")
-  paperText.font = Font.mediumRoundedSystemFont(18)
-    
-  // Last Artikel Title
-  const lastArtikel = column.addText(apiData.items[0].title)
-  lastArtikel.font = Font.mediumRoundedSystemFont(14)
-  lastArtikel.textColor = new Color("#00CD66")
-  widget.addSpacer(0)
+ if (widgetSize != 'small') {
+   logoStack.addSpacer(130)
+   list.setPadding(15, 25, 5, 25)
+ }
+ else {
+   logoStack.addSpacer(10)
+   list.setPadding(5, 5, 5, 5)
+ }
 
-  // string has max length for date 
-  let apiDatePub = apiData.items[0].date_published
-  var strDate = apiDatePub.substr(0, 10);
-  console.log(date) 
-  console.log(apiDatePub) 
-  
-  // Last Artikel published date
-  const lastArtDate = column.addText(strDate)
-  lastArtDate.font = Font.mediumRoundedSystemFont(10)
-  
-  if (date == apiDatePub) {
-    // row two
-    const row2 = widget.addStack()
-    row2.layoutVertically()
+ const logoImageStack = logoStack.addStack()
+ logoStack.layoutHorizontally()
+ logoImageStack.backgroundColor = new Color("#ffffff", 1.0)
+ logoImageStack.cornerRadius = 6
+ const wimg = logoImageStack.addImage(logoImg)
 
-    const newArti = row2.addText("New Artikel")
-    newArti.font = Font.regularSystemFont(14)
-    newArti.textColor = new Color("#00CD66")
-    }else{
-     // row two
-    const row2 = widget.addStack()
-    row2.layoutVertically()
+  if (widgetSize != 'small') {
+   wimg.imageSize = new Size(50, 50)
+   wimg.rightAlignImage()
+ }else {
+   wimg.imageSize = new Size(40, 40)
+   wimg.rightAlignImage()
+ }
 
-    const newArt = row2.addText(apiData.description)
-    newArt.font = Font.regularSystemFont(10)
-  }
+
+ const paperText = column1.addText("notiz.Blog")
+ if (widgetSize != 'small') {
+   paperText.font = Font.mediumRoundedSystemFont(20)
+ }else {
+   paperText.font = Font.mediumRoundedSystemFont(18)
 }
 
+ // Last Artikel Title
+ const lastArtikel = list.addText(apiData.items[0].title)
+if (widgetSize != 'small') {
+   lastArtikel.font = Font.mediumRoundedSystemFont(18)
+ }else {
+   lastArtikel.font = Font.mediumRoundedSystemFont(16)
+ }
 
+ lastArtikel.textColor = new Color("#00CD66")
+
+ // string has max length for date 
+ let apiDatePub = apiData.items[0].date_published
+ var strDate = apiDatePub.substr(0, 10)
+
+if (widgetSize != 'small') {
+   console.log("Bride size get txt!")
+
+   // Last Artikel published date
+   const lastArtDate = column1.addText(strDate)
+   lastArtDate.font = Font.mediumRoundedSystemFont(15)
+
+   const row2 = list.addStack()  
+   row2.layoutVertically()
+
+   if (date == apiDatePub) {
+     const street = list.addText("New Artikel")
+     street.font = Font.regularSystemFont(14)
+     street.textColor = new Color("#00CD66")
+   }else{
+     const newArt = list.addText(apiData.items[0].content_text)  
+     newArt.font = Font.regularSystemFont(10)
+   }
+ }else {
+  console.log("Small size no txt!")
+  const showDate = list.addText("Published at: " + " " + strDate)
+  showDate.font = Font.mediumRoundedSystemFont(12)
+
+  list.addSpacer(4)
+
+  const publisher = list.addText("Author: " + apiData.items[0].author.name)
+  publisher.font = Font.mediumRoundedSystemFont(10)
+ }
+
+ return list
+}
+
+//------------------------------------------------
 // url get json https://notiz.blog/feed/json
 async function getNewCasesData() {
-  
-  let url = "https://notiz.blog/feed/json"
-  let req = new Request(url)
-  let apiResult = await req.loadJSON()
-  
-  return apiResult
+
+ let url = "https://notiz.blog/feed/json"
+ let req = new Request(url)
+ let apiResult = await req.loadJSON()
+
+ return apiResult
 }
 
-
+//------------------------------------------------
+// get date to compare with articel date
 async function getDate() {
 
-  let currentTime = new Date().toLocaleTimeString('de-DE', { hour: "numeric", minute: "numeric" })
-  let currentDay = new Date().getDate();
-  let currentMonth = new Date().getUTCMonth()+1;
-  let currentYear = new Date().getFullYear();
-  
-  let date = (currentYear + "-" + currentMonth + "-" + currentDay); 
-  
-//     console.log("Date: " + date)
-  
-    return date
+ let currentTime = new Date().toLocaleTimeString('de-DE', { hour: "numeric", minute: "numeric" })
+ let currentDay = new Date().getDate();
+ let currentMonth = new Date().getUTCMonth()+1;
+ let currentYear = new Date().getFullYear();
+
+ let date = (currentYear + "-" + currentMonth + "-" + currentDay); 
+
+ return date
 }
 
+//------------------------------------------------
 // get images from local filestore or download them once
 async function getImage(image) {
-   let fm = FileManager.local()
-   let dir = fm.documentsDirectory()
-   let path = fm.joinPath(dir, image)
-   if (fm.fileExists(path)) {
-       return fm.readImage(path)
-    } else {
-        // download once
-        let imageUrl
-        switch (image) {
-            case 'logo.png':
-                imageUrl = "https://notiz.blog/wp-content/uploads/2019/10/cropped-notizblog-icon-1-180x180.png"
-                break
-            default:
-                console.log(`Sorry, couldn't find ${image}.`);
-        }
-        let iconImage = await loadImage(imageUrl)
-        fm.writeImage(path, iconImage)
-        return iconImage
-    }
+  let fm = FileManager.local()
+  let dir = fm.documentsDirectory()
+  let path = fm.joinPath(dir, image)
+  if (fm.fileExists(path)) {
+      return fm.readImage(path)
+   } else {
+       // download once
+       let imageUrl
+       switch (image) {
+           case 'logo.png':
+               imageUrl = "https://notiz.blog/wp-content/uploads/2019/10/cropped-notizblog-icon-1-180x180.png"
+               break
+           default:
+               console.log(`Sorry, couldn't find ${image}.`);
+       }
+       let iconImage = await loadImage(imageUrl)
+       fm.writeImage(path, iconImage)
+       return iconImage
+   }
 }
 
-
+//------------------------------------------------
 // helper function to download an image from a given url
 async function loadImage(imgUrl) {
-   const req = new Request(imgUrl)
-   return await req.loadImage()
+  const req = new Request(imgUrl)
+  return await req.loadImage()
 }
 
+//------------------------------------------------
+// For Checking Frame Size
+async function presentAlert(prompt,items,asSheet) 
+{
+ let alert = new Alert()
+ alert.message = prompt
+
+ for (const item of items) {
+   alert.addAction(item)
+ }
+ let resp = asSheet ? 
+   await alert.presentSheet() : 
+   await alert.presentAlert()
+ return resp
+}
 
 // end of script copy until here
